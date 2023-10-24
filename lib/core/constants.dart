9a10a1c55b625ddb3a3be487c204be05605e3a22
brunoverc <bruno@teckavan.com>
2023-10-24 09:18:14 -0300

    First commit

---
+++ lib/core/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const Color PLAYER1_COLOR = Colors.red;
+const Color PLAYER2_COLOR = Colors.blue;
+
+const String PLAYER1_SYMBOL = 'X';
+const String PLAYER2_SYMBOL = '0';
+
+const String GAME_TITLE = 'Jogo da Velha';
+const String TIED_TITLE = 'Jogo empatado!';
+const String WIN_TITLE = 'Jogador [SYMBOL] venceu!';
+const String DIALOG_MESSAGE = 'Pressione o botão de reset para iniciar novamente';
+const String SINGLE_PLAYER_MODE_LABEL = '1 Jogador';
+const String MULTIPLAYER_MODEL_LABEL = '2 Jogadores';
+const String RESET_BUTTON_LABEL = 'RESET';
---
+++ lib/models/board_tile_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BoardTile {
+  final int id;
+  String symbol;
+  Color color;
+  bool enable;
+
+  BoardTile(this.id,
+      {this.symbol = '', this.color = Colors.black26, this.enable = true});
+}
